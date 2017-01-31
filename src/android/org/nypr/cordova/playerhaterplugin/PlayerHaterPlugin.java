29e800a7f68429489e8a8fe6d2bf3e32d0927e58
Rocky Kitamura <rockykitamura@gmail.com>
2017-01-30 23:01:00 -0800

    Stop audio on destroy.  Keep screen awake

--- src/android/org/nypr/cordova/playerhaterplugin/PlayerHaterPlugin.java
+++ src/android/org/nypr/cordova/playerhaterplugin/PlayerHaterPlugin.java
@@ -23,6 +23,7 @@ import android.net.ConnectivityManager;
 import android.os.Environment;
 import android.os.RemoteException;
 import android.util.Log;
+import android.view.WindowManager;
 
 
 public class PlayerHaterPlugin extends CordovaPlugin implements OnAudioInterruptListener, OnAudioStateUpdatedListener{
@@ -60,6 +61,8 @@ public class PlayerHaterPlugin extends CordovaPlugin implements OnAudioInterrupt
 		// }
 		// mCachedWebView = webView;
 
+		cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
 		Log.d(LOG_TAG, "PlayerHater Plugin initialized");
 	}
 
@@ -68,6 +71,15 @@ public class PlayerHaterPlugin extends CordovaPlugin implements OnAudioInterrupt
 		Log.d(LOG_TAG, "PlayerHater Plugin onReset");
 		_checkForWakeup(cordova.getActivity().getIntent()); // invoked when the activity is freshly launched...
 		super.onReset();
+	}
+
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (mAudioPlayer != null) {
+			mAudioPlayer.stopPlaying();
+		}
 	}
 
 	@Override
